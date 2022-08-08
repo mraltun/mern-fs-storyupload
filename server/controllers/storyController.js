be6f3d9a3f0253f64e7998ee126294e84a383608
Murat Altun <mraltun@gmail.com>
2022-08-08 13:46:00 +0300

    Implemented getStory and createStory in storyController for backend

--- server/controllers/storyController.js
+++ server/controllers/storyController.js
@@ -1,5 +1,22 @@
-// Controllers
+import mongoose from "mongoose";
+import Story from "../models/storyModel.js";
 
 export const getStory = async (req, res) => {
-  res.send("Hello");
+  try {
+    const story = await Story.find();
+    res.status(200).json(story);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+export const createStory = async (req, res) => {
+  const body = req.body;
+  const newStory = new Story({ ...body });
+  try {
+    await newStory.save();
+    res.status(201).json(newStory);
+  } catch (err) {
+    res.status(409).json({ message: err.message });
+  }
 };
--- server/routes/storyRoutes.js
+++ server/routes/storyRoutes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { getStory } from "../controllers/storyController.js";
+import { getStory, createStory } from "../controllers/storyController.js";
 const router = Router();
 
 router.get("/", getStory);
+router.post("/", createStory);
 
 export default router;
