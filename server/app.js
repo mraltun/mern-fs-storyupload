68b51e8ee56af29f020142a6dd383c9cf718e003
Murat Altun <mraltun@gmail.com>
2022-08-08 12:57:03 +0300

    Body Parser added

--- server/app.js
+++ server/app.js
@@ -2,3 +2,10 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+// Body parsing middleware.
+app.use(bodyParser.json({ limit: "32mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "32mb", extended: true }));
+app.use(cors());
