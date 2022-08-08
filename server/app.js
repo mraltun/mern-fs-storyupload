64291218b65cca97a4b996c6394e9d2e2652848e
Murat Altun <mraltun@gmail.com>
2022-08-08 13:04:25 +0300

    Added database connections and mongoose on

--- server/app.js
+++ server/app.js
@@ -9,3 +9,23 @@ const app = express();
 app.use(bodyParser.json({ limit: "32mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "32mb", extended: true }));
 app.use(cors());
+
+const MONGO_URI =
+  "mongodb+srv://mraltun:aa12345600@cluster0.btiys.mongodb.net/mernfsstoryuploaddb";
+const PORT = process.env.PORT || 5000;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+  } catch (err) {
+    console.log("Connection to MongoDB failed", err.message);
+  }
+};
+
+connectDB();
+
+mongoose.connection.on("open", () =>
+  console.log("Connection to database is successfull")
+);
+mongoose.connection.on("error", (err) => console.log(err));
