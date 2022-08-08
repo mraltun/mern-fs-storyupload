f2932288d1c55b935f179a27aa714f0518cce956
Murat Altun <mraltun@gmail.com>
2022-08-08 17:09:39 +0300

    StoryForm implemented file upload

--- client/src/components/StoryForm/StoryForm.jsx
+++ client/src/components/StoryForm/StoryForm.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card, Form, Input, Typography, Button } from "antd";
+import FileBase64 from "react-file-base64";
 import styles from "../../assets/styles/StoryForm.styles";
 
 const { Title } = Typography;
@@ -33,6 +34,20 @@ const StoryForm = () => {
         </Form.Item>
         <Form.Item name='tags' label='Tags'>
           <Input.TextArea allowClear autoSize={{ minRows: 2, maxRows: 6 }} />
+        </Form.Item>
+        <Form.Item name='image' label='Image' rules={[{ required: true }]}>
+          <FileBase64
+            type='file'
+            multiple={false}
+            onDone={(e) => {
+              form.setFields({ image: e.base64 });
+            }}
+          />
+        </Form.Item>
+        <Form.Item wrapperCol={{ span: 16, offset: 6 }}>
+          <Button type='primary' block htmlType='submit'>
+            Share
+          </Button>
         </Form.Item>
       </Form>
     </Card>
