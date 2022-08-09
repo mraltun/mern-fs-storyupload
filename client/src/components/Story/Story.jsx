18302eaa1804788867e24ebe124a8bfef1fd0d30
Murat Altun <mraltun@gmail.com>
2022-08-09 13:50:28 +0300

    Implemented listing in StoryList and Story components

--- client/src/components/Story/Story.jsx
+++ client/src/components/Story/Story.jsx
@@ -1,8 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import moment from "moment";
+import { Card, Tooltip, Typography, Image } from "antd";
+import { EditOutlined, DeleteTwoTone, HeartTwoTone } from "@ant-design/icons";
 import styles from "../../assets/styles/Story.styles";
 
-const Story = () => {
-  return <div>Story</div>;
+const { Meta } = Card;
+const { Link, Paragraph, Text } = Typography;
+
+const Story = ({ story }) => {
+  const [expand, setExpand] = useState(true);
+
+  return (
+    <Card
+      style={styles.card}
+      cover={<Image src={story.image} />}
+      actions={[
+        <div style={styles.actions}>
+          <Tooltip
+            placement='top'
+            title='Like'
+            color='magenta'
+            onClick={() => {}}
+          >
+            <HeartTwoTone twoToneColor='magenta' />
+            &nbsp; {story.likes} &nbsp;
+          </Tooltip>
+        </div>,
+        <Tooltip placement='top' title='Edit'>
+          <EditOutlined onClick={() => {}} />
+        </Tooltip>,
+        <Tooltip placement='top' title='Delete' color=''>
+          <DeleteTwoTone twoToneColor='red' onClick={() => {}} />
+        </Tooltip>,
+      ]}
+    >
+      <Meta title={story.username} />
+      <Paragraph
+        style={{ margin: 0 }}
+        ellipsis={{
+          rows: 2,
+          expandable: true,
+          symbol: "more",
+          onExpand: () => {
+            setExpand(true);
+          },
+          onEllipsis: () => {
+            setExpand(false);
+          },
+        }}
+      >
+        {story.caption}
+      </Paragraph>
+      {expand ? (
+        <Link href='#'>{story.tags.split(" ").map((tag) => `#${tag} `)}</Link>
+      ) : null}
+      <br />
+      <Text type='secondary'>{moment(story.postDate).fromNow()}</Text>
+    </Card>
+  );
 };
 
 export default Story;
--- client/src/components/StoryList/StoryList.jsx
+++ client/src/components/StoryList/StoryList.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Row } from "antd";
+import { Row, Col, Spin } from "antd";
 import Story from "../Story";
 
 const StoryList = () => {
   const stories = useSelector((state) => state.stories);
-  return (
+
+  return !stories.length ? (
+    <div style={{ textAlign: "center" }}>
+      <Spin size='large' />
+    </div>
+  ) : (
     <Row gutter={[48, 32]}>
-      <Story />
-      <Story />
+      {stories.map((story) => {
+        return (
+          <Col key={story._id} lg={24} xl={12} xxl={8}>
+            <Story story={story} />
+          </Col>
+        );
+      })}
     </Row>
   );
 };
