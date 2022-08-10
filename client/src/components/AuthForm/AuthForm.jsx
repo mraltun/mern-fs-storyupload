2c8cc98c851c2e7db621c61d43bdb002977d80fa
Murat Altun <mraltun@gmail.com>
2022-08-10 13:26:30 +0300

    Added form element to AuthForm

--- client/src/components/AuthForm/AuthForm.jsx
+++ client/src/components/AuthForm/AuthForm.jsx
@@ -1,8 +1,51 @@
 import React from "react";
+import { Form, Typography, Button, Input, Card, Layout } from "antd";
+import { UserOutlined, LockOutlined, MailOutlined } from "@ant-design/icons";
 import styles from "../../assets/styles/AuthForm.styles";
 
+const { Title } = Typography;
+
 const AuthForm = () => {
-  return <div>AuthForm</div>;
+  const user = null;
+  const [form] = Form.useForm();
+
+  const handlerFinish = () => {};
+
+  const isLogin = false;
+
+  return (
+    <Layout style={styles.container}>
+      <Card
+        style={styles.card}
+        title={
+          <Title level={4} style={{ textAlign: "center" }}>
+            {isLogin ? "Login" : "Join"} Story Upload
+          </Title>
+        }
+      >
+        <Form
+          name='authform'
+          form={form}
+          size='large'
+          wrapperCol={{ span: 20, offset: 2 }}
+          onFinish={handlerFinish}
+        >
+          {isLogin || (
+            <>
+              <Form.Item
+                name='username'
+                rules={[
+                  { required: true, message: "Please enter your username" },
+                ]}
+              >
+                <Input prefix={<UserOutlined />} placeholder='username' />
+              </Form.Item>
+            </>
+          )}
+        </Form>
+      </Card>
+    </Layout>
+  );
 };
 
 export default AuthForm;
