23e9fcca81d36c5ad889ae0ab609efd1bed896f0
Murat Altun <mraltun@gmail.com>
2022-08-10 15:19:38 +0300

    Implemented login and registeration form in AuthForm

--- client/src/components/AuthForm/AuthForm.jsx
+++ client/src/components/AuthForm/AuthForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Typography, Button, Input, Card, Layout } from "antd";
 import { UserOutlined, LockOutlined, MailOutlined } from "@ant-design/icons";
 import styles from "../../assets/styles/AuthForm.styles";
@@ -8,10 +8,13 @@ const { Title } = Typography;
 const AuthForm = () => {
   const user = null;
   const [form] = Form.useForm();
+  const [isLogin, setIsLogin] = useState(true);
 
-  const handlerFinish = () => {};
+  const handleFinish = () => {};
 
-  const isLogin = false;
+  const handleClick = () => {
+    setIsLogin((prevIsLogin) => !prevIsLogin);
+  };
 
   return (
     <Layout style={styles.container}>
@@ -19,7 +22,7 @@ const AuthForm = () => {
         style={styles.card}
         title={
           <Title level={4} style={{ textAlign: "center" }}>
-            {isLogin ? "Login" : "Join"} Story Upload
+            {isLogin ? "Login" : "Join"} to Story Upload
           </Title>
         }
       >
@@ -28,7 +31,7 @@ const AuthForm = () => {
           form={form}
           size='large'
           wrapperCol={{ span: 20, offset: 2 }}
-          onFinish={handlerFinish}
+          onFinish={handleFinish}
         >
           {isLogin || (
             <>
@@ -38,10 +41,50 @@ const AuthForm = () => {
                   { required: true, message: "Please enter your username" },
                 ]}
               >
-                <Input prefix={<UserOutlined />} placeholder='username' />
+                <Input prefix={<UserOutlined />} placeholder='Username' />
               </Form.Item>
             </>
           )}
+          <Form.Item
+            name='email'
+            rules={[{ required: true, message: "Please enter your email" }]}
+          >
+            <Input prefix={<MailOutlined />} placeholder='Email' />
+          </Form.Item>
+
+          <Form.Item
+            name='password'
+            rules={[{ required: true, message: "Please enter your password" }]}
+          >
+            <Input.Password
+              type='password'
+              prefix={<LockOutlined />}
+              placeholder='Password'
+            />
+          </Form.Item>
+          {isLogin || (
+            <Form.Item
+              name='confirmPassword'
+              rules={[
+                { required: true, message: "Please repeat your password" },
+              ]}
+            >
+              <Input.Password
+                type='password'
+                prefix={<LockOutlined />}
+                placeholder='Confirm Password'
+              />
+            </Form.Item>
+          )}
+          <Form.Item>
+            <Button htmlType='submit' typeof='primary'>
+              {isLogin ? "Login" : "Join"}
+            </Button>
+            <span style={{ margin: "0 10px 20px 0" }}>Or</span>
+            <Button type='link' onClick={handleClick}>
+              {isLogin ? "Register" : "Have an account?"}
+            </Button>
+          </Form.Item>
         </Form>
       </Card>
     </Layout>
