e3b0482e328662308ef13127ea1e8424eb28baf1
Murat Altun <mraltun@gmail.com>
2022-08-10 11:49:43 +0300

    Added packages for user authentication

--- client/src/actions/StoryAction.js
+++ client/src/actions/StoryAction.js
@@ -10,8 +10,7 @@ import {
 export const getStories = () => async (dispatch) => {
   try {
     const { data } = await api.fetchStories();
-    const action = { type: FETCH_ALL_STORIES, payload: data };
-    dispatch(action);
+    dispatch({ type: FETCH_ALL_STORIES, payload: data });
   } catch (error) {
     console.log(error.message);
   }
