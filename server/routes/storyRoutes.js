6643b23c093bca54fd90450ac1b9d8ad6fe5c754
Murat Altun <mraltun@gmail.com>
2022-08-10 19:25:19 +0300

    Added authMiddleware

--- server/routes/storyRoutes.js
+++ server/routes/storyRoutes.js
@@ -6,12 +6,14 @@ import {
   deleteStory,
   likeStory,
 } from "../controllers/storyController.js";
+
+import authentication from "../middlewares/authMiddleware.js";
 const router = Router();
 
 router.get("/", getStory);
-router.post("/", createStory);
-router.patch("/:id", updateStory);
-router.delete("/:id", deleteStory);
-router.patch("/:id/likeStory", likeStory);
+router.post("/", authentication, createStory);
+router.patch("/:id", authentication, updateStory);
+router.delete("/:id", authentication, deleteStory);
+router.patch("/:id/likeStory", authentication, likeStory);
 
 export default router;
