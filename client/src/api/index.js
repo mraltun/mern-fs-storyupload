1648d5c799efce6939fbaa80ccae6637263f5fb7
Murat Altun <mraltun@gmail.com>
2022-08-11 20:14:17 +0300

    Added interceptor for the token

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,6 +2,15 @@ import axios from "axios";
 
 const api = axios.create({ baseURL: "http://localhost:5000" });
 
+api.interceptors.request.use((req) => {
+  if (localStorage.getItem("profile")) {
+    const profile = JSON.parse(localStorage.getItem("profile"));
+    req.headers.Authorization = `Bearer ${profile.token}`;
+  }
+
+  return req;
+});
+
 export const fetchStories = () => api.get("/stories");
 export const createStory = (story) => api.post("/stories", story);
 export const updateStory = (id, story) => api.patch(`/stories/${id}`, story);
